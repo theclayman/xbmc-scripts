9dcd7be0953085c45742fd5f957a4bb152ce8af2
theclayman <theclayman@gmx.de>
2014-02-20 16:49:43 +0100

    Liste mit Unterschieden erzeugen

--- movielist.py
+++ movielist.py
@@ -6,6 +6,7 @@ import os
 import locale
 import sys
 import csv
+import itertools
 
 # Ideen:
 # - in Dateinamen das Jahr ergänzen, das in der XML-Datei steht.
@@ -18,14 +19,69 @@ import csv
 def main():
     args = parseargs()
 
-    outfilename = getoutfilename(args.xmlfilename)
+    #outfilename = getoutfilename(args.xmlfilename)
     #if(os.path.exists(outfilename)):
     #    print("Ausgabedatei existiert bereits und wird nicht ueberschrieben. Bitte Eingabedatei anders benennen.")
     #    sys.exit(1)
 
-    movies = parsexml(args.xmlfilename)
+    movies1 = parsexml(args.xmlfilename1)
+    movies2 = parsexml(args.xmlfilename2)
 
-    exportmovielist(movies, outfilename)
+    difflist = comparemovies(movies1, movies2)
+    exportdifflist(difflist)
+
+    #exportmovielist(movies, outfilename)
+
+def exportdifflist(difflist):
+    # bevor itertools.groupby angewendet wird, muss die Liste sortiert werden,
+    # weil groupby immer dann eine neue Gruppe beginnt, wenn sich der definierte
+    # Schlüssel vom einen zum anderen ändert (im Gegensatz zu SQL group by)
+    difflist = sorted(difflist, key = lambda x: x.difftype)
+    with codecs.open("difflist.txt", "w", encoding = "UTF-8") as outfile:
+        for group, movielist in itertools.groupby(difflist, lambda x: x.difftype):
+            outfile.write("%s\n" % group)
+            for entry in sorted(movielist, key = lambda x: x.movie.Title):
+                outfile.write("  %s (%s)\n" % (entry.movie.Title, entry.movie.Year))
+
+
+def comparemovies(movies1, movies2):
+    # Welche Filme sind in der einen Liste enthalten, aber nicht in der anderen?
+    # d.h.: welche Filme kann man tauschen?
+    results = []
+    for movie in movies2:
+        # habe ich den Film schon?
+        findresult = findmovie(movie, movies1)
+        if(findresult != None):
+            results.append(findresult)
+
+    return results
+
+def findmovie(movie, movielist):
+    foundmovies = list(filter(lambda m: (movie.Title == m.Title) and (movie.Year == m.Year), movielist))
+
+    if(len(foundmovies) == 0):
+        # Film wurde in der Liste nicht gefunden, er fehlt also in meiner Sammlung
+        return FindResult("NEW", movie)
+    elif(len(foundmovies) == 1):
+        # Film ist in meiner Sammlung schon vorhanden, aber jetzt prüft man,
+        # ob er in der anderen Sammlung in besserer Qualität vorhanden ist
+        newmovie = foundmovies[0]
+        if( (movie.ResolutionWidth * movie.ResolutionHeight) >
+            (newmovie.ResolutionWidth * newmovie.ResolutionHeight)):
+                return FindResult("RESOLUTION", movie)
+    else:
+        print("Film ist in Sammlung doppelt vorhanden: %s" % movie)
+        return FindResult("DUPLICATE", movie)
+
+
+
+class FindResult:
+    def __init__(self, difftype, movie):
+        self.difftype = difftype
+        self.movie = movie
+
+    def __str__(self):
+        return "%s %s" % (self.difftype, self.movie)
 
 def getoutfilename(xmlfilename):
     filename = os.path.basename(xmlfilename)
@@ -94,7 +150,6 @@ class Movie:
     def __str__(self):
         return "%s (%s) [%s]  %sx%s %s" % (self.Title, self.OriginalTitle, self.Year, self.ResolutionWidth, self.ResolutionHeight, self.resolutionSymbol())
 
-
     def resolutionSymbol(self):
         """Liefert in Abhängigkeit der Auflösung eine Abkürzung hierfür zurück: FullHD, HD, SD, ?"""
         if(self.ResolutionHeight == 0 or self.ResolutionWidth == 0):
@@ -109,7 +164,8 @@ class Movie:
 
 def parseargs():
     parser = argparse.ArgumentParser("Export-Skript für XBMC-Bibliothek")
-    parser.add_argument("-x", "--xmlfile", help="Eingabedatei im XML-Format", dest="xmlfilename")
+    parser.add_argument("-x1", "--xmlfile1", help="erste Filmliste im XML-Format", dest="xmlfilename1")
+    parser.add_argument("-x2", "--xmlfile2", help="zweite Filmliste im XML-Format", dest="xmlfilename2")
     args = parser.parse_args()
     return args
 
